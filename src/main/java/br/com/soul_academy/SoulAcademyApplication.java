ea8c6af2e63aed16703288ff09c8ff134e39c7f7
Cleiton <cabeca.cl@gmail.com>
2023-01-30 15:19:16 -0300

    Criação do projeto Soul Academy - Inicio de tudo

---
+++ src/main/java/br/com/soul_academy/SoulAcademyApplication.java
@@ -0,0 +1,13 @@
+package br.com.soul_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/soul_academy/soul_academy/SoulAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soul_academy.soul_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
