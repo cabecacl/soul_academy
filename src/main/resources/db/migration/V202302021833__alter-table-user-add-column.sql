7788139dd03a4e51f4eaaf874e9a09526731d9cb
Jonas Araujo <jonaslaraujo@gmail.com>
2023-02-05 09:43:14 -0300

    Corrigindo as migrations

--- src/main/resources/db/migration/V202302021833__alter-table-user-add-column.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE user
-    ADD COLUMN id_people bigint not null  COMMENT 'Id da tabela People';
-ALTER TABLE user
-    ADD CONSTRAINT fk_user_people FOREIGN KEY (id_people) REFERENCES people(id_people);
